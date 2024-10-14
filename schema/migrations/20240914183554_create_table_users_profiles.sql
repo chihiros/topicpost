663440dcd2254fa8247dda337d9aae66cfb3dd45
chihiros <szrkw2ui2a@gmail.com>
2024-10-14 13:53:53 +0900

    Refactor users_profiles table: Update column name and comments

--- schema/migrations/20240914183554_create_table_users_profiles.sql
+++ schema/migrations/20240914183554_create_table_users_profiles.sql
@@ -2,14 +2,14 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users_profiles (
   id SERIAL,
-  user_uid UUID NOT NULL PRIMARY KEY,
+  users_uid UUID NOT NULL PRIMARY KEY,
   profiles_id INT NOT NULL,
   created_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMENT ON TABLE users_profiles IS 'ユーザーとプロフィールの中間テーブル';
-COMMENT ON COLUMN users_profiles.user_uid IS 'ユーザーID';
+COMMENT ON COLUMN users_profiles.users_uid IS 'ユーザーID';
 COMMENT ON COLUMN users_profiles.profiles_id IS 'プロフィールID';
 COMMENT ON COLUMN users_profiles.created_at IS '作成日時';
 COMMENT ON COLUMN users_profiles.updated_at IS '更新日時';
